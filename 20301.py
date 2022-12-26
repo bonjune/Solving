4a153edc2000ababe27b4e0e56677a5e228244e3
Bongjun Jang <malloc099@gmail.com>
2022-12-26 13:17:19 +0900

    Solved 20301

--- 20301.py
+++ 20301.py
@@ -1,34 +1,20 @@
+from collections import deque
+
 N, K, M = map(int, input().split())
 
-class Josephus:
-    def __init__(self) -> None:
-        self.people = list(range(1, N+1))
-        self.cnt = 0
-        self.i = 0
-        self.dir = 1
+m = 0
+dir = True
+queue = deque(range(1, N+1))
+
+while len(queue):
+    if dir:
+        for _ in range(K-1):
+            queue.append(queue.popleft())
+    else:
+        for _ in range(K):
+            queue.appendleft(queue.pop())
     
-    def start(self):
-        global N
-        while self.cnt < N:
-            self._next()
-    
-    def _next(self):
-        global N, K, M
-
-        i, j = 0, 0
-        while abs(i) < K:
-            if self.people[(self.i + j) % N] is None:
-                j += self.dir
-                continue
-            j += self.dir
-            i += self.dir
-        self.i = (self.i + j - self.dir) % N
-        print(self.people[self.i])
-        self.people[self.i] = None
-        self.cnt += 1
-        self.dir = self.dir if self.cnt % M else -self.dir
-
-        
-
-j = Josephus()
-j.start()
+    print(queue.popleft())
+    m += 1
+    if m % M == 0:
+        dir = not dir
